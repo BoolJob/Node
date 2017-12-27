4058f9c38d049747767131c131e9284d7807edfc
Javier Parra <xavi@localhost.localdomain>
2017-12-27 17:08:25 -0300

    test

---
+++ pull_git.sh
@@ -0,0 +1,6 @@
+cd /home/xavi/Proyectos/booljob-api
+git pull origin master
+
+cd /home/xavi/Proyectos/bd
+git pull origin master
+
---
+++ push_git.sh
@@ -0,0 +1,5 @@
+git add .
+git commit -m "commit javier"
+git remote add origin https://github.com/BoolJob/Node.git
+git push -u origin master
+
